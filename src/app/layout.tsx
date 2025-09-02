de64005585ac0e9a8f5a5f1084adf7bff0125ab9
Rumeet Goradia <rumeet.goradia@gmail.com>
2025-09-01 20:50:40 -0400

    Add fonts and skeleton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Geist, EB_Garamond } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
 
@@ -13,14 +13,19 @@ export const metadata: Metadata = {
 
 const geist = Geist({
   subsets: ["latin"],
-  variable: "--font-geist-sans",
+          variable: "--font-geist-sans",
+});
+
+const eb_garamond = EB_Garamond({
+subsets: ["latin"],
+variable: "--font-eb-garamond"
 });
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en" className={`${geist.variable} ${eb_garamond.variable}`}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
