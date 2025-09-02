385ff6e537423f9329c13a971302bab42a59b95c
Rumeet Goradia <rumeet.goradia@gmail.com>
2025-09-01 20:55:52 -0400

    Update branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default async function Home() {
   return (
-    <main className="bg-primary-400 flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-5xl">Pooja & Yash</h1>
-      <p>Please come to our wedding.</p>
+    <main className="bg-primary text-primary-foreground flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl">Pooja & Yash</h1>
+      <p>June 19, 2026</p>
     </main>
   );
 }
