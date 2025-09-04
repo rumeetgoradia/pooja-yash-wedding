fa81db7a52ababf2bc1b4caff6631bd07efe817e
Rumeet Goradia <rumeet.goradia@gmail.com>
2025-09-03 22:09:24 -0400

    Add some better styling for hero

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import Image from "next/image";
+import Image, { type StaticImageData } from "next/image";
 import { useState, useEffect } from "react";
+import { cn } from "~/lib/utils";
 
-interface HeroProps {
-  src: string;
+export interface HeroImageProps {
+  src: StaticImageData;
   alt: string;
+  className: string;
+}
+
+export interface HeroProps {
+  image: HeroImageProps;
   children: React.ReactNode;
 }
 
-export const Hero: React.FC<HeroProps> = ({ src, alt, children }) => {
+export const Hero: React.FC<HeroProps> = ({
+  image: { src, alt, className },
+  children,
+}) => {
   const [blur, setBlur] = useState(0);
 
   useEffect(() => {
@@ -29,26 +38,27 @@ export const Hero: React.FC<HeroProps> = ({ src, alt, children }) => {
   }, []);
 
   return (
-    <section className="relative flex h-[75vh] items-center justify-center overflow-hidden">
-      <div className="absolute inset-0 z-10 bg-gradient-to-b from-black to-transparent" />
+    <div className="bg-black pt-24">
+      <section className="relative flex h-[75vh] items-center justify-center overflow-hidden">
+        <div className="absolute inset-0 z-10 bg-gradient-to-b from-black to-transparent" />
+        <div
+          className="transition-filter absolute inset-0 h-full w-full duration-300"
+          style={{ filter: `blur(${blur}px)` }}
+        >
+          <Image
+            src={src}
+            alt={alt}
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            priority
+            placeholder="blur"
+            className={cn(className)}
+          />
+        </div>
 
-      <div
-        className="transition-filter absolute inset-0 h-full w-full duration-300"
-        style={{ filter: `blur(${blur}px)` }}
-      >
-        <Image
-          src={src}
-          alt={alt}
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-          priority
-        />
-      </div>
-
-      <div className="relative z-10 px-4 text-center text-white">
-        {children}
-      </div>
-    </section>
+        <div className="relative z-10 w-full p-12">{children}</div>
+      </section>
+    </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import { Hero } from "./hero";
+import { Hero, type HeroImageProps } from "./hero";
+import LandingImage from "public/Landing.jpg";
+
+const HeroImage: HeroImageProps = {
+  src: LandingImage,
+  alt: "Pooja & Yash",
+  className: "object-[82.5%] md:object-center",
+};
 
 export default async function Home() {
   return (
     <main>
-      <Hero src="/Landing.jpg" alt="Pooja & Yash">
-        <h1 className="text-5xl">Pooja & Yash</h1>
+      <Hero image={HeroImage}>
+        <h1 className="text-left text-5xl text-white">Pooja & Yash</h1>
       </Hero>
       <p className="h-[2000px]">Lorem ipsum</p>
     </main>
