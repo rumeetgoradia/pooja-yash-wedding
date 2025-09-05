ffa3e524de9925cc208a1de1b7431cf9ed5ec120
Rumeet Goradia <rumeet.goradia@gmail.com>
2025-09-05 15:29:42 -0400

    Update landing text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,10 @@ const HeroImage: HeroImageProps = {
   className: "object-[82.5%] md:object-center",
 };
 
+const AndLine = () => {
+  return <hr className="border-primary w-[64px] border md:w-[96px]" />;
+};
+
 export default async function Home() {
   return (
     <main>
@@ -14,15 +18,19 @@ export default async function Home() {
       <h2 className="sr-only">June 19, 2026 ⋅ Naples, Florida</h2>
       <Hero image={HeroImage}>
         <div className="flex flex-col items-center justify-center gap-2 font-serif leading-0">
-          <div className="text-8xl font-light text-white uppercase">Pooja</div>
-          <div className="-mt-2 flex w-full items-center justify-center gap-8">
-            <hr className="border-primary w-[5vw] border" />
-            <div className="shrink-0 text-4xl font-thin text-white italic">
+          <div className="text-6xl font-light text-white uppercase md:text-8xl">
+            Pooja
+          </div>
+          <div className="-mt-2 flex w-full items-center justify-center gap-4 md:gap-8">
+            <AndLine />
+            <div className="shrink-0 text-2xl font-thin text-white italic md:text-4xl">
               and
             </div>
-            <hr className="border-primary w-[5vw] border" />
+            <AndLine />
           </div>
-          <div className="text-8xl font-light text-white uppercase">Yash</div>
+          <div className="text-6xl font-light text-white uppercase md:text-8xl">
+            Yash
+          </div>
         </div>
       </Hero>
       <p className="h-[2000px]">Lorem ipsum</p>
