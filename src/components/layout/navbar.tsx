131301b2cc629e9b6bf98eb28c082f33435e13f8
Rumeet Goradia <rumeet.goradia@gmail.com>
2025-09-06 11:12:24 -0400

    Move components

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import Logo from "../logo";
+import Logo from "~/components/logo";
 import { usePathname } from "next/navigation";
 import { cn } from "~/lib/utils";
 import { Menu, X } from "lucide-react";
