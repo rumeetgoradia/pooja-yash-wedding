b73a010ba4440feddac698cd58e4525f4431ef72
Rumeet Goradia <rumeet.goradia@gmail.com>
2025-10-06 21:42:36 -0400

    Update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import "~/styles/globals.css";
 
 import { type Metadata } from "next";
 import {
-  Cormorant_Garamond,
-  Geist,
-  Lavishly_Yours,
-  Newsreader,
+    Bodoni_Moda,
+    Geist,
+    Lavishly_Yours,
+    Newsreader,
 } from "next/font/google";
 import localFont from "next/font/local";
 
@@ -14,41 +14,41 @@ import Navbar from "~/components/layout/navbar";
 import Background from "~/components/layout/background";
 
 export const metadata: Metadata = {
-  title: "Pooja & Yash",
-  description: "Wedding of Pooja and Yash. June 19, 2026.",
-  icons: [{ rel: "icon", url: "/favicon/favicon.ico" }],
+    title: "Pooja & Yash",
+    description: "Wedding of Pooja and Yash. June 19, 2026.",
+    icons: [{ rel: "icon", url: "/favicon/favicon.ico" }],
 };
 
 const sans = Geist({
-  subsets: ["latin"],
-  variable: "--font-custom-sans",
+    subsets: ["latin"],
+    variable: "--font-custom-sans",
 });
 
-const serif = Cormorant_Garamond({
-  subsets: ["latin"],
-  variable: "--font-custom-serif",
+const serif = Bodoni_Moda({
+    subsets: ["latin"],
+    variable: "--font-custom-serif",
 });
 
 const script = localFont({
-  src: "../../public/fonts/CitadelScriptStd.otf",
-  variable: "--font-custom-script",
-  weight: "400",
+    src: "../../public/fonts/CitadelScriptStd.otf",
+    variable: "--font-custom-script",
+    weight: "400",
 });
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html
-      lang="en"
-      className={`${sans.variable} ${serif.variable} ${script.variable}`}
-    >
-      <body>
-        <TRPCReactProvider>
-          <Navbar />
-          {children}
-        </TRPCReactProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html
+            lang="en"
+            className={`${sans.variable} ${serif.variable} ${script.variable}`}
+        >
+            <body>
+                <TRPCReactProvider>
+                    <Navbar />
+                    {children}
+                </TRPCReactProvider>
+            </body>
+        </html>
+    );
 }
