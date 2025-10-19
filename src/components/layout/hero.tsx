253ceb9e406b50514246c7b23ecf304fd8a38b4c
Rumeet Goradia <rumeet.goradia@gmail.com>
2025-10-19 12:50:30 -0400

    Remove hero sizes

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -32,7 +32,6 @@ export const Hero: React.FC<HeroProps> = ({
                         src={src}
                         alt={alt}
                         fill
-                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                         objectFit="cover"
                         quality={100}
                         priority
